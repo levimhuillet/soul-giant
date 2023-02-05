7bab565d3be16294b3b4c07a0b01f4fd1cfaf61a
levimhuillet <levimhuillet@gmail.com>
2023-02-05 15:30:59 -0600

    stomach complete

---
+++ Assets/_Code/World/Triggers/RaiseObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaiseObject : MonoBehaviour
+{
+    [SerializeField] private GameObject m_ToRaise;
+    [SerializeField] private float m_RaiseSpeed;
+
+    private enum RiseState { 
+        Idle,
+        Rising
+    }
+
+    private RiseState m_State;
+
+    private void Start() {
+        m_State = RiseState.Idle;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player")) {
+            m_State = RiseState.Rising;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_State == RiseState.Rising) {
+            m_ToRaise.transform.position += new Vector3(0, m_RaiseSpeed * Time.deltaTime, 0);
+        }
+    }
+}
